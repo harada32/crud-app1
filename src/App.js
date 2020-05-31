43f1d8107127ba3e6e86b171aafbc900f7c86d1b
harada32 <haradaaaaaaaaa@gmail.com>
2020-05-31 14:52:36 +1200

    components update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 
 const App = () => {
+  const profiles = [
+    { name: "Taro", age:"21"},
+    { name: "Yusaku", age:"27"}
+  ]
+
   return(
     <React.Fragment>
-      <Cat />
-      <Cat />
+      {
+        profiles.map((profile, index) => {
+          return <User key={index} name={profile.name} age={profile.age}/>
+        })
+      }
     </React.Fragment>
   )};
 
-const Cat = () => <div>Meow!</div>
+const User = (props) => <div>{props.name}! {props.age} years old.</div>
 
 export default App;
